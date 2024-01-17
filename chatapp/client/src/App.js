735c6d7aaf1e200f6bbc6ba78980425d9ef64cd9
Dorette <dorette.schutte86@gmail.com>
2024-01-17 08:46:17 +0200

    chatapp commit

---
+++ chatapp/client/src/App.js
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from '@mui/material/Button';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { TextField } from "@mui/material";
+
+function App() {
+  const [socket, setSocket] = useState(null)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    setSocket(io('http://localhost:4000'))
+  }, []);
+
+  function handleForm(e) {
+    e.preventDefault();
+    socket.emit("send-message", { message });
+    setMessage("");
+  }
+
+  return (
+    <div>
+      <Box component="form" onSubmit={handleForm}>
+        <TextField 
+        size="small" 
+        label="Write your message" 
+        variant="standard"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button variant="text" type="submit">Send</Button>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ chatapp/server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+
+const app = express();
+const PORT = 4000;
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["http://localhost:3000"],
+    },
+});
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+   // res.json({ data: "hello world from socket" });
+   res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+    //console.log('Connection is ready');
+    socket.on("send-message", (data) => {
+        socket.emit("message-from-server", data);
+    })
+});
+
+httpServer.listen(PORT, () => {
+    console.log("Server is running at http://localhost:4000")
+});
