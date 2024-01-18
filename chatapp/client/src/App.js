f9913e23959cdb2e2a3ced119bb15df1c274e4dc
Dorette <dorette.schutte86@gmail.com>
2024-01-18 08:49:31 +0200

    adding code

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -1,36 +1,29 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Button from '@mui/material/Button';
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
-import { TextField } from "@mui/material";
+import Cookies from "js-cookies";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { io } from "socket.io-client";
+import Header from "./components/Header";
 
 function App() {
-  const [socket, setSocket] = useState(null)
-  const [message, setMessage] = useState('')
+  const [socket, setSocket] = useState(null);
+  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
-    setSocket(io('http://localhost:4000'))
+    setSocket(io("http://localhost:4000"));
+    const _userId = Cookies.getItem("userId");
+    if (_userId) setUserId(_userId);
   }, []);
-
-  function handleForm(e) {
-    e.preventDefault();
-    socket.emit("send-message", { message });
-    setMessage("");
-  }
 
   return (
     <div>
-      <Box component="form" onSubmit={handleForm}>
-        <TextField 
-        size="small" 
-        label="Write your message" 
-        variant="standard"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        />
-        <Button variant="text" type="submit">Send</Button>
-      </Box>
+      <Container>
+        <Header socket={socket} userId={userId} setUserId={setUserId} />
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Outlet context={{ socket, userId }} />
+        </Box>
+      </Container>
     </div>
   );
 }
--- chatapp/server/index.js
+++ chatapp/server/index.js
@@ -26,8 +26,19 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     //console.log('Connection is ready');
     socket.on("send-message", (data) => {
-        socket.emit("message-from-server", data);
-    })
+        socket.broadcast.emit("message-from-server", data);
+    });
+
+    socket.on("typing-started", () => {
+        socket.broadcast.emit("typing-started-from-server");
+    });
+
+    socket.on('disconnect', (socket) => {
+        console.log('User left.');
+    //  socket.on("send-message", (data) => {
+    //    socket.emit("message-from-server", data);
+    //})
+    });
 });
 
 httpServer.listen(PORT, () => {
